3054909c6010b34186ed7612793a15e528529bc3
srividya815 <65211233+srividya815@users.noreply.github.com>
2020-05-20 12:09:47 +0530

    20-05-20

---
+++ ReverseStringPreservingOrder.java
@@ -0,0 +1,24 @@
+package com.vids.may19;
+
+public class ReverseStringPreservingOrder {
+
+	public static void main(String[] args) {
+		//String input = "abcd";
+		String input = "I am a java developer";
+		String[] words = input.split(" ");
+		//Single word as input so reverse chars
+		if(words.length==1){
+			char[] chars = input.toCharArray();
+			for(int i=chars.length-1; i>=0; i--){
+				System.out.print(chars[i]);
+			}
+		}
+		else{
+			for(int i=words.length-1; i>=0; i--){
+				System.out.print(words[i]+" ");
+			}
+		}
+
+	}
+
+}
